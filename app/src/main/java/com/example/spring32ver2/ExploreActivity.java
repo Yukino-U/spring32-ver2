05106dc337bc10574530aaf7ea5e70c4aaa7a0ba
双葉 <futabamusic20@gmail.com>
2023-09-22 09:27:54 +0900

    探検機能の1ページ目の時間をボタンで変更できるようにする

--- app/src/main/java/com/example/spring32ver2/ExploreActivity.java
+++ app/src/main/java/com/example/spring32ver2/ExploreActivity.java
@@ -3,7 +3,17 @@ package com.example.spring32ver2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ExploreActivity extends AppCompatActivity {
 
@@ -23,6 +33,45 @@ public class ExploreActivity extends AppCompatActivity {
             startActivity(intent);
 
         });
+
+        Button upButton = findViewById(R.id.button2);
+        Button downButton = findViewById(R.id.button);
+
+
+        upButton.setOnClickListener(v -> {
+            EditText editText = findViewById(R.id.editTextTime2);
+            String str = editText.getText().toString();
+            DateFormat formatter = new SimpleDateFormat("mm:ss");
+            try {
+                Date time = formatter.parse(str);
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(time);
+                calendar.add(Calendar.MINUTE, -1);
+                SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
+                editText.setText(sdf.format(calendar.getTime()));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+        });
+        downButton.setOnClickListener(v -> {
+            EditText editText = findViewById(R.id.editTextTime2);
+            String str = editText.getText().toString();
+
+
+            DateFormat formatter = new SimpleDateFormat("mm:ss");
+            try {
+                Date time = formatter.parse(str);
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(time);
+                calendar.add(Calendar.MINUTE, 1);
+                SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
+                editText.setText(sdf.format(calendar.getTime()));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        });
+
 
     }
 }
